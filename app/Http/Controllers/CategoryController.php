c67d2e9bbb6df8574833c8478f0f47df4c344ed5
Mark Rawlinson <mr@willis-organs.com>
2017-01-03 23:07:32 +0000

    Finish Inserting many-to-many

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,7 +9,7 @@ class CategoryController extends Controller
 {
     public function show(Category $category)
     {
-        $products = $category->products()->orderBy('created_at', 'desc')->paginate(1);
+        $products = $category->products()->orderBy('created_at', 'desc')->paginate(10);
         
         return view('category.show', compact('category', 'products'));
     }
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,57 @@
 |
 */
 
+use App\Product;
+use App\Category;
+
 Route::get('/', function () {
-    return view('welcome');
+    
+    // create new product and make connection in category_product pivot table
+
+        // $product = new Product;
+        // $product->title = 'Apple Mouse';
+        // $product->price = 40;
+
+        // $product->save();
+
+    // associates product to category and appends it in category_product table
+        // $product->categories()->attach($category);
+
+    
+    // associates existing product to category and appends it in category_product table
+    // to create link in pivot table
+        // $product = Product::find(4);
+
+        // $category = Category::find(1);
+
+        // $product->categories()->attach($category);
+
+    // to create link in pivot table for multiple categories (existing product)
+        // $product = Product::find(4);
+
+        // $category = Category::find([1, 2]);
+        // $category = Category::whereIn('title', ['Tech', 'Apple'])->get();
+
+        // $product->categories()->attach($category);
+
+    // to remove link from pivot table
+        // $product = Product::find(4);
+
+        // $category = Category::find(1);
+
+        // $product->categories()->detach($category);
+
+    // to remove all products from a category
+        // $category = Category::find(1);
+
+        // $product = Product::find([4, 5]);
+
+        // $category->products()->detach($product);
+
+    // if you know e.g. product ids use sync to attach (also avoids duplication)
+        // $category = Category::find(1);
+
+        // $category->products()->sync([4, 5]);
 });
 
 Auth::routes();
